c0c6894f1e8b541a31815cd590e1955d365779aa
timphip <146890276+timphip@users.noreply.github.com>
2023-12-21 09:44:29 -0400

    Add files via upload

--- Q1.c
+++ Q1.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 int main() {
 
-  // These variables were created and had a starting value for you, no need to change
-  char bookVersionReview = 'A';
-  char movieVersionReview = 'B';
-  double ticketPrice = 10.25;
-  double bookPrice = 19.99;
+	// These variables were created and had a starting value for you, no need to change
+	char bookVersionReview = 'A';
+	char movieVersionReview = 'B';
+	double ticketPrice = 10.25;
+	double bookPrice = 19.99;
 
-  // Update the movie review score here// Update the movie review score here （在这里更新电影的评分）
-
-
-  // Update the ticket price here// Update the ticket price here （在这里更新电影票价格）
+	// Update the movie review score here (在这里更新电影的评分)
 
 
-  // No need to change below here// No need to change below here 
-  printf("The book version has a review score of %c and costs $%.2fprintf("The book version has a review score of %c and costs $%.2f\\n", bookVersionReview, n", bookVersionReview, bookPrice);bookPrice);
-  printf("The movie version has a review score of %c and costs $%.2fprintf("The movie version has a review score of %c and costs $%.2f\\n", movieVersionReview, n", movieVersionReview, ticketPrice);ticketPrice);
+	// Update the ticket price here (在这里更新电影票价格)
+
+	
+
+	// No need to change below here
+	printf("The book version has a review score of %c and costs $%.2fprintf("The book version has a review score of %c and costs $%.2f\\n", bookVersionReview, n", bookVersionReview, bookPrice);bookPrice);
+	printf("The movie version has a review score of %c and costs $%.2fprintf("The movie version has a review score of %c and costs $%.2f\\n", movieVersionReview, n", movieVersionReview, ticketPrice);ticketPrice);
 }
+
